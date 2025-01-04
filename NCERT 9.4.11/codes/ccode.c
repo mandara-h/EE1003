b6356e1bd8fa872c6e1cd96cfdfd38f70997435c
mandara-h <mandara.hosur@gmail.com>
2025-01-04 10:15:50 +0530

    Assignment 1 - second commit

--- NCERT 9.4.11/codes/ccode.c
+++ NCERT 9.4.11/codes/ccode.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <math.h>
 
-float y_val(float x_n, float y_n, float h){
+float y(float x_n, float y_n, float h){
 	return (y_n + (h * (2*pow(x_n,2) + x_n) / (pow(x_n,3) + pow(x_n,2) + x_n + 1)));
 }
 
-float x_val(float x_n, float h){
+float x(float x_n, float h){
 	return (x_n + h);
+}
+
+float y_val(float x){
+	return ((0.25 * log(pow(x+1,2)*pow(pow(x,2)+1,3))) - (0.5 * atan(x)) + 1);
 }
--- NCERT 9.4.11/codes/code.py
+++ NCERT 9.4.11/codes/code.py
@@ -7,32 +7,39 @@ from ctypes import *
 vals = CDLL('./vals.so')
 
 #assigning the C functions in Python
-x_val = vals.x_val
-y_val = vals.y_val
+x_func = vals.x
+y_func = vals.y
+y_act = vals.y_val
 
 #setting the result type of the C-Python functions
-x_val.restype = c_float
-y_val.restype = c_float
+x_func.restype = c_float
+y_func.restype = c_float
+y_act.restype = c_float
 
 #setting initial coordinates as the point given in the question
 x = 0.0
 y = 1.0
-plt.plot(x,y,'ro')
+plt.plot(x,y,'ro', label='Simulation Plot')
+plt.plot(x,y,'y+', label='Theoretical Plot')
 
 #initialising h
 h = 0.1
 
 #iterating to generate more points on the curve using finite differences and plotting each indivdual point
 for i in range(100):
-    y = y_val(c_float(x), c_float(y), c_float(h))
-    x = x_val(c_float(x), c_float(h))
-    plt.plot(x,y,'ro')
+    y = y_func(c_float(x), c_float(y), c_float(h)) #y-coordinate of simulation plot
+    y_val = y_act(c_float(x)) #y-coordinate of theoretical plot
+    x = x_func(c_float(x), c_float(h)) #x-coordinate of simulation plot and theoretical plot
+    plt.plot(x,y,'ro') #plotting simulation curve
+    plt.plot(x,y_val,'y+') #plotting theoretical curve
 
 #setting up the labels and grid for the plot
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
-plt.title('Approximate Plot')
 plt.grid(True)
+
+#adding a legend
+plt.legend()
 
 #saving the generated plot
 plt.savefig('../figs/fig.pdf')
